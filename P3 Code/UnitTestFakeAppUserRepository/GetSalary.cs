d110e0b8981ce3786bbdb6187608d5708285b4bd
Xavier Pavlis <Xavier.Pavlis@trojans.dsu.edu>
2020-09-17 15:04:40 -0500

    Added the unit test.

---
+++ P3 Code/UnitTestFakeAppUserRepository/GetSalary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Deployment.Internal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using P3_Code;
+
+namespace UnitTestFakeAppUserRepository
+{
+    [TestClass]
+    public class GetSalary
+    {
+        [TestMethod]
+        public void GetSalaryWhenGoodIdCorrect()
+        {
+            //Arrange
+            const int ID = 1;
+            FakeAppUserRepository IAppUser = new FakeAppUserRepository();
+
+            //Act
+
+            //Assert
+        }
+    }
+}
