962809e83bcc5f888e39978dba24801cf87c6d45
Xavier Pavlis <Xavier.Pavlis@trojans.dsu.edu>
2020-09-17 15:51:06 -0500

    Completed the two Unit tests coding

--- P3 Code/UnitTestFakeAppUserRepository/GetSalary.cs
+++ P3 Code/UnitTestFakeAppUserRepository/GetSalary.cs
@@ -6,18 +6,44 @@ using P3_Code;
 namespace UnitTestFakeAppUserRepository
 {
     [TestClass]
-    public class GetSalary
+    public class Save
     {
         [TestMethod]
-        public void GetSalaryWhenGoodIdCorrect()
+        public void SaveUserWorks()
         {
             //Arrange
-            const int ID = 1;
-            FakeAppUserRepository IAppUser = new FakeAppUserRepository();
+            const string First_Name = "Test";
+            const string Last_Name = "Case";
+            const string Email_Address = "Test@testcase.com";
+            FakeAppUserRepository AppUser = new FakeAppUserRepository();
 
             //Act
+            AppUser user = new AppUser();
+            user.FirstName = First_Name;
+            user.LastName = Last_Name;
+            user.EmailAddress = Email_Address;
+            int id = FakeAppUserRepository.Save(user);
+            AppUser newUser = FakeAppUserRepository.GetByUserName(id);
 
             //Assert
+            Assert.IsTrue(user.FirstName == newUser.FirstName && user.LastName == newUser.LastName && user.EmailAddress == newUser.EmailAddress);
+        }
+
+    }
+    public class GetEmail
+    {
+        public void GetEmailWhenIdIsCorrect()
+        {
+            //Arrange
+            const int id = 1;
+            const string Expected_Email = "Test@testcase.com";
+            FakeAppUserRepository newUser = new FakeAppUserRepository();
+
+            //Act
+            string Actual_Email = FakeAppUserRepository.GetEmail(id);
+
+            //Assert
+            Assert.AreEqual(Expected_Email, Actual_Email);
         }
     }
 }
