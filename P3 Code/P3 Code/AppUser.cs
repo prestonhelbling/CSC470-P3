ebf45288f4d80abea9e8e6d38af7917aca0c93e7
Preston Helbling <preston.helbling@trojans.dsu.edu>
2020-09-16 19:43:43 -0500

    - Test Commit

---
+++ P3 Code/P3 Code/AppUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_Code
+{
+    //AppUser Class
+    class AppUser
+    {
+        string UserName;
+        string Password;
+        string FirstName;
+        string LastName;
+        string EmailAddress;
+        bool IsAuthenticated;
+
+    }
+}
---
+++ P3 Code/P3 Code/IAppUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_Code
+{
+    //IAppUser Interface
+    interface IAppUser
+    {
+        bool Login(string Username, string Password);
+        List<AppUser> GetAll();
+        void SetAuthentication(string UserName, bool IsAuthenticated);
+        AppUser GetByUserName(string Username);
+    }
+}
